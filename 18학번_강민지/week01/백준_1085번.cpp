73647c11c8e37f8d0ecd2f5318b38cb59eecce68
kmj3785 <jan3785@naver.com>
2020-05-07 23:49:10 +0900

    first

---
+++ 18학번_강민지/week01/백준_1085번.cpp
@@ -0,0 +1,12 @@
+// 201824403 KangMinJi
+// 1984kb, 0ms
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int x, y, w, h;
+    scanf("%d%d%d%d",&x,&y,&w,&h);
+    printf("%d",min(w-x,min(x,min(h-y,y))));
+    return 0;
+}
---
+++ 18학번_강민지/week01/백준_1110번.cpp
@@ -0,0 +1,19 @@
+// 201824403 KangMinJi
+// 1994kb, 0ms
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int num, current_num = -1;
+    int cycle = 0;
+    scanf("%d", &num);
+    while(num!=current_num) {
+        if(cycle==0)
+            current_num = num;
+        current_num = current_num%10*10 + (current_num/10+current_num%10)%10;
+        cycle++;
+    }
+    printf("%d\n", cycle);
+    return 0;
+}
